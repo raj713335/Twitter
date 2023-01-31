65705dabb25b6caab92add7bdf2059ef2de00b68
ARNAB DAS <raj713335@gmail.com>
2023-02-01 02:44:30 +0530

    Section 2: Using NPM

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const displayContent = require("./routes");
+const colors = require('colors');
 
 const server = http.createServer(displayContent);
 
 server.listen(3001);
+
+console.log('hello'.green);
 
