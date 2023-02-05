91fbe991d9c10ac429628ba0424e09b4f33a5a6c
ARNAB DAS <raj713335@gmail.com>
2023-02-05 07:25:50 +0530

    Section 7: Styling the posts

--- public/js/common.js
+++ public/js/common.js
@@ -1,7 +1,6 @@
 $("#postTextarea").keyup(event => {
     var textbox = $(event.target);
     var value = textbox.val().trim();
-    console.log(value)
 
     var submitButton = $("#submitPostButton");
 
@@ -14,3 +13,62 @@ $("#postTextarea").keyup(event => {
 
     submitButton.prop("disabled", false);
 })
+
+$("#submitPostButton").click(() => {
+    var button = $(event.target);
+    var textbox = $("#postTextarea");
+
+    var data = {
+        content: textbox.val()
+    }
+
+    $.post("/api/posts", data, (postData) => {
+        var html = createPostHtml(postData);
+        $(".postsContainer").prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
+    })
+})
+
+
+function createPostHtml(postData) {
+
+    var postedBy = postData.postedBy;
+    var displayName = postedBy.firstName + " " + postedBy.lastName;
+    var timestamp = postData.createdAt;
+
+    return `<div class='post'>
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}'>
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <a href='/profile/${postedBy.userName}' class='displayName'>${displayName}</a>
+                            <span class='username'>@${postedBy.userName}</span>
+                            <span class='date'>${timestamp}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-comment'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-retweet'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-heart'></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
