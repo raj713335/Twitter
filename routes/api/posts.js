57748559ecb4b492ae3c0c3352fc488cbfb033ab
ARNAB DAS <raj713335@gmail.com>
2023-02-12 18:33:21 +0530

    Section 11: Replying to posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,20 +8,19 @@ const Post = require('../../schemas/PostSchema');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
 
-    Post.find()
-    .populate("postedBy")
-    .populate("retweetData")
-    .sort({ "createdAt": -1 })
-    .then(async results => {
-        results = await User.populate(results, { path: "retweetData.postedBy" })
-        res.status(200).send(results);
-    })
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
+    var results = await getPosts({});
+    res.status(200).send(results);
+})
+
+router.get("/:id", async (req, res, next) => {
+
+    var postId = req.params.id;
+
+    var results = await getPosts({ _id: postId });
+    results = results[0];
+    res.status(200).send(results);
     
 })
 
@@ -35,6 +34,10 @@ router.post("/", async (req, res, next) => {
     var postData = {
         content: req.body.content,
         postedBy: req.session.user
+    }
+
+    if(req.body.replyTo){
+        postData.replyTo = req.body.replyTo;
     }
 
     Post.create(postData)
@@ -121,5 +124,17 @@ router.post("/:id/retweet", async (req, res, next) => {
 
     res.status(200).send(post);
 })
+
+async function getPosts(filter) {
+    var results = await Post.find(filter)
+    .populate("postedBy")
+    .populate("retweetData")
+    .populate("replyTo")
+    .sort({ "createdAt": -1 })
+    .catch(error =>  console.log(error)) 
+    
+    results = await User.populate(results, { path: "replyTo.postedBy" });
+    return await User.populate(results, { path: "retweetData.postedBy" });
+}
 
 module.exports = router;
