c80185ec026f4c6c79fc0023d149134ae5b8e6e1
ARNAB DAS <raj713335@gmail.com>
2023-02-22 02:26:12 +0530

    Section 17: Uploading a cover photo

--- public/js/common.js
+++ public/js/common.js
@@ -112,6 +112,30 @@ $("#filePhoto").change(function() {
     }
 })
 
+$("#coverPhoto").change(function() { 
+    // var input = $(event.target);
+    
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = (e) => {
+            var image = document.getElementById("coverPreview");
+            image.src = e.target.result;
+
+            // $("#imagePreview").attr("src", e.target.result);
+
+            if(cropper !== undefined) {
+                cropper.destroy();
+            }
+
+            cropper = new Cropper(image, {
+                aspectRatio: 16 / 9,
+                background: false
+            });
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
+})
+
 $("#imageUploadButton").click(() => {
     var canvas = cropper.getCroppedCanvas();
 
@@ -126,6 +150,30 @@ $("#imageUploadButton").click(() => {
 
         $.ajax({
             url: "/api/users/profilePicture",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: () => location.reload()
+        })
+    })
+
+})
+
+$("#coverPhotoButton").click(() => {
+    var canvas = cropper.getCroppedCanvas();
+
+    if(canvas == null) {
+        alert("could not upload image. Make sure it is an image file.");
+        return;
+    }
+
+    canvas.toBlob((blob) => {
+        var formData = new FormData();
+        formData.append("croppedImage", blob);
+
+        $.ajax({
+            url: "/api/users/coverPhoto",
             type: "POST",
             data: formData,
             processData: false,
